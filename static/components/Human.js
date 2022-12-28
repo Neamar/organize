82211eeebaeae5fdee0c0b34aa97b42ca80bc245
Neamar <neamar@neamar.fr>
2022-12-28 16:14:01 +0100

    Save assignments

--- static/components/Human.js
+++ static/components/Human.js
@@ -2,9 +2,15 @@ export default {
   props: {
     data: Object,
   },
+  methods: {
+    unassign() {
+      this.data.assignment = null;
+    },
+  },
   template: `
   <div>
-    <p>Nom : {{ data.name }}</p>
+    <p>Nom : {{ data.name }} <span v-if="data.assignment">({{ data.assignment.mission.name }}) <button @click="unassign">Annuler</button></span>
+    </p>
   </div>
   `,
 };
--- static/components/PendingMission.js
+++ static/components/PendingMission.js
@@ -16,14 +16,18 @@ export default {
       const selectedId = parseInt(e.srcElement.value);
       const human = this.humans.find((h) => h.id === selectedId);
       human.assignment = this;
+      e.srcElement.value = '';
+    },
+    unassign(participant) {
+      participant.assignment = null;
     },
   },
   template: `
   <div>
     <p>Mission : {{ mission.name }}</p>
     <ul>
-    <li v-for="participant in participants">{{ participant.name }}</li>
-    <li><select @change="appendParticipant">
+    <li v-for="participant in participants">{{ participant.name }} <button @click="unassign(participant)">Annuler</button></li>
+    <li v-if="potentialParticipants.length > 0"><select @change="appendParticipant">
       <option selected value="">--</option>
       <option v-for="human in potentialParticipants" :value="human.id">{{ human.name }}</option>
     </select>
